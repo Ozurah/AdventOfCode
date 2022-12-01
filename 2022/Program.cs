60bad3e41a9876a321551089ffdc68d6cbe77ea4
Jonas Allemann <jonas.allemann@he-arc.ch>
2022-12-01 21:47:42 +0100

    chore: setup repo

---
+++ 2022/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+// Hello World! program
+namespace AdventOfCode2022
+{
+    class Program {         
+        static void Main(string[] args)
+        {
+            Day1.prob1();
+        }
+    }
+}
---
+++ 2022/day1/Day1.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Linq;
+
+// Hello World! program
+namespace AdventOfCode2022
+{
+    class Day1 {         
+        public static void prob1()
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
