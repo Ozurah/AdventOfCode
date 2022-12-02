595d6e4c341ed6cc5c464b3ed315d234b1d5726d
Ozurah <80265286+Ozurah@users.noreply.github.com>
2022-12-02 18:03:08 +0100

    feat: day 2

--- 2022/Day2.cs
+++ 2022/Day2.cs
@@ -16,6 +16,7 @@ namespace AdventOfCode2022
 
         enum ScoreResult
         {
+            // result = score
             Win = 6,
             Draw = 3,
             Lose = 0,
@@ -28,15 +29,29 @@ namespace AdventOfCode2022
             { Shape.Scissors, Shape.Paper },
         };
 
-        private static Dictionary<String, Shape> mapAction = new Dictionary<string, Shape>()
-            {
-                { "A", Shape.Rock },
-                { "B", Shape.Paper },
-                { "C", Shape.Scissors },
-                { "X", Shape.Rock },
-                { "Y", Shape.Paper },
-                { "Z", Shape.Scissors },
-            };
+        private static Dictionary<String, Shape> mapActionProb1 = new Dictionary<string, Shape>()
+        {
+            { "A", Shape.Rock },
+            { "B", Shape.Paper },
+            { "C", Shape.Scissors },
+            { "X", Shape.Rock },
+            { "Y", Shape.Paper },
+            { "Z", Shape.Scissors },
+        };
+
+        private static Dictionary<String, Shape> mapActionProb2 = new Dictionary<string, Shape>()
+        {
+            { "A", Shape.Rock },
+            { "B", Shape.Paper },
+            { "C", Shape.Scissors },
+        };
+
+        private static Dictionary<String, ScoreResult> mapResultProb2 = new Dictionary<string, ScoreResult>()
+        {
+            { "X", ScoreResult.Lose },
+            { "Y", ScoreResult.Draw },
+            { "Z", ScoreResult.Win },
+        };
 
         /// <summary>
         /// 
@@ -56,7 +71,33 @@ namespace AdventOfCode2022
             {
                 var play = line.Split(" ");
                 // Assuming the input is in the map (no non-existing key)
-                round.Add(new Tuple<Shape, Shape>(mapAction[play[0]], mapAction[play[1]]));
+                // improvement would be by using .TryGetValue or LINQ .FirstOrDefault
+                round.Add(new Tuple<Shape, Shape>(mapActionProb1[play[0]], mapActionProb1[play[1]]));
+            }
+
+            return round;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="player1"></param>
+        /// <param name="player2"></param>
+        /// <returns>Tuple : Opponent, My</returns>
+        private static List<Tuple<Shape, ScoreResult>> initProb2()
+        {
+            String file = Program.inputPath + @"/day2.txt";
+
+            List<String> lines = System.IO.File.ReadAllLines(file).ToList();
+
+            var round = new List<Tuple<Shape, ScoreResult>>();
+
+            foreach (String line in lines)
+            {
+                var play = line.Split(" ");
+                // Assuming the input is in the map (no non-existing key)
+                // improvement would be by using .TryGetValue or LINQ .FirstOrDefault
+                round.Add(new Tuple<Shape, ScoreResult>(mapActionProb2[play[0]], mapResultProb2[play[1]]));
             }
 
             return round;
@@ -84,7 +125,22 @@ namespace AdventOfCode2022
 
         public static void prob2()
         {
+            var rounds = initProb2();
 
+            int sum = 0;
+            foreach (var round in rounds)
+            {
+                sum += (int)round.Item2;
+
+                if (round.Item2 == ScoreResult.Draw)
+                    sum += (int)round.Item1; // Same result as played by opponent
+                else if (round.Item2 == ScoreResult.Lose)
+                    sum += (int)winAgainst[round.Item1];
+                else
+                    sum += (int)winAgainst.First(x => x.Value == round.Item1).Key;
+            }
+
+            Console.WriteLine(sum);
         }
     }
 }
--- README.md
+++ README.md
@@ -1,9 +1,20 @@
 # AdventOfCode
 Advent of code calandar resolution
 
+&nbsp;&nbsp;:sparkles:&emsp;&nbsp;&nbsp;&nbsp;:star:&nbsp;&nbsp;:dizzy:
+&emsp;:star2:&nbsp;&nbsp;:full_moon_with_face:&emsp;&emsp;:star:
+:sparkles:&emsp;&emsp;&nbsp;:star2:&emsp;:sparkles:
+&emsp;&emsp;&nbsp;:snowflake:&emsp;:snowflake:
+&emsp;&nbsp;:snowflake:&emsp;:snowman:
+&emsp;&emsp;:gift::christmas_tree::gift:
+
 ## 2022 - C#
 https://adventofcode.com/2022/
 
 Day | Stars
 ---|---
 1 | :star::star:
+2 | :star::star:
+
+:tada::confetti_ball::confetti_ball::tada:
+:cocktail::cake::tropical_drink::cake:
